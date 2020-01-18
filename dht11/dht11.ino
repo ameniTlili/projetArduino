cfbae95b5eb93bf05e1cb53f6bbb0020cabf5e72
ameniTlili <ameni1tlili@gmail.com>
2020-01-18 15:57:17 +0100

    Add files via upload

---
+++ dht11/dht11.ino
@@ -0,0 +1,26 @@
+#include "DHT.h"
+
+#define DHTPIN 4    
+
+#define DHTTYPE DHT11   
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+  dht.begin();
+}
+
+void loop() {
+   delay(2000);
+  float h = dht.readHumidity();
+
+  float t = dht.readTemperature();
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print("%  Temperature: ");
+  Serial.print(t);
+  Serial.print("°C ");
+ 
+}
